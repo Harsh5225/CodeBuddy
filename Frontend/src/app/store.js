373bff89052c7ce750cd456683fd29339b36c897
Harsh <harshsingh94070@gmail.com>
2025-06-21 20:31:30 +0530

    update: added submissionApi to store configuration

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import problemReducer from "../features/problem/problemSlice";
-
+import chartReducer from "../features/chatMessage/ChatSlice";
+import { submissionApi } from "../features/submission/submissionApi";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     problem: problemReducer,
+    chat: chartReducer,
+    [submissionApi.reducerPath]: submissionApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(submissionApi.middleware),
 });
-"// Updated configuration" 
+("// Updated configuration");
