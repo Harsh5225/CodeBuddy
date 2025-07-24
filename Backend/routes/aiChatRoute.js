3954c69661f354754f05ba3cd469330fcce019d9
Harsh <harshsingh94070@gmail.com>
2025-07-24 23:04:27 +0530

    chat-route

--- Backend/routes/aiChatRoute.js
+++ Backend/routes/aiChatRoute.js
@@ -1,7 +1,10 @@
 import express from "express";
 import solveDoubt from "../controllers/solveDoubt.controller.js";
+import { userMiddleware } from "../middlewares/userMiddleware.js";
+import { checkAIAccess, incrementAIUsage } from "../middlewares/subscriptionMiddleware.js";
+
 const router = express.Router();
 
-router.post("/chat", solveDoubt);
+router.post("/chat", userMiddleware, checkAIAccess, solveDoubt);
 
 export default router;
