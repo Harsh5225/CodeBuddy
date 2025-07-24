6d4fc9d07560de835ce6a089ca78d47d08403a07
Harsh <harshsingh94070@gmail.com>
2025-07-24 23:05:57 +0530

    watch-problemId route

--- Backend/routes/videoCreator.js
+++ Backend/routes/videoCreator.js
@@ -1,19 +1,22 @@
 import express from "express";
 import { adminMiddleware } from "../middlewares/adminMiddleware.js";
+import { checkVideoAccess } from "../middlewares/subscriptionMiddleware.js";
+
 const videoRouter = express.Router();
-// const {
-//   generateUploadSignature,
-//   saveVideoMetadata,
-//   deleteVideo,
-// } = require("../controllers/videoSection");
 
 import {
   generateUploadSignature,
   saveVideoMetadata,
   deleteVideo,
+  getVideoForUser,
 } from "../controllers/videoSection.controller.js";
+
+// Admin routes (no subscription check needed)
 videoRouter.get("/create/:problemId", adminMiddleware, generateUploadSignature);
-videoRouter.post("/save", adminMiddleware,saveVideoMetadata);
+videoRouter.post("/save", adminMiddleware, saveVideoMetadata);
 videoRouter.delete("/delete/:problemId", adminMiddleware, deleteVideo);
+
+// User routes (require subscription check)
+videoRouter.get("/watch/:problemId", checkVideoAccess, getVideoForUser);
 
 export default videoRouter;
