47f17d5d7e996011648d0b43d805063cd4ea0fa3
Harsh <harshsingh94070@gmail.com>
2025-07-25 21:39:43 +0530

    Fronted-url-deployed updated

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 const server = http.createServer(app);
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL||"http://localhost:5173",
   credentials: true, // ✅ allow cookies
 };
 
