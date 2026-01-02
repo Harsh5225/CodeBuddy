ef4096d60acb0539901e69cb19235750e2a97241
Harsh <harshsingh94070@gmail.com>
2026-01-03 01:17:02 +0530

    fix: support REDIS_URL for robust render deployment

--- Backend/database/redis.js
+++ Backend/database/redis.js
@@ -1,14 +1,19 @@
 import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
-const client = createClient({
-  username: "default",
-  password: process.env.REDIS_PASSWORD,
-
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT || 17998,
-  },
-});
+const client = createClient(
+  process.env.REDIS_URL
+    ? {
+      url: process.env.REDIS_URL,
+    }
+    : {
+      username: "default",
+      password: process.env.REDIS_PASSWORD,
+      socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT || 17998,
+      },
+    }
+);
 
 export default client;
